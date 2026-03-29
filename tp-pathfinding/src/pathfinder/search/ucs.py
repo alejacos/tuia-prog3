d6c2fc9fc8edae1f46cde801337a629ae19af95b
alejacos <aleacosta969@gmail.com>
2026-03-29 16:18:52 -0300

    Se agregan cambios en UCS. Queda funcionando

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -24,6 +24,107 @@ class UniformCostSearch:
 
         # Initialize frontier with the root node
         # TODO Complete the rest!!
-        # ...
+        frontier = PriorityQueueFrontier()
+        frontier.add(root,root.cost)
 
-        return NoSolution(reached)
+        #Iterar mientras la frontera no esté vacía
+        while True:
+            if frontier.is_empty():
+                return NoSolution(reached)
+            
+            #Retiro un nodo de la frontera
+            node = frontier.pop()
+
+            #Verificar Test objetivo
+            if grid.objective_test(node.state):
+                return Solution(node, reached)
+
+            #Aplicar todas las acciones posibles <---------------------
+            # Checkear si going "right" es posible
+            if "right" in grid.actions(node.state):
+                # Obtener el succesor  y su costo
+                successor = grid.result(node.state, "right")
+                cost = node.cost + grid.individual_cost(node.state, "right")
+
+                # Checkear si el sucesor está en alcanzados o si el costo es menor
+                if successor not in reached or cost < reached[successor]:
+                    # Initialize the son node
+                    son = Node(
+                        "",
+                        successor,
+                        cost=cost,
+                        parent=node,
+                        action="right",
+                    )
+
+                    # Cargar el sucesor como alcanzado con su costo
+                    reached[successor] = cost
+                    #Encolar el nodo en la frontera con su costo
+                    frontier.add(son, cost)
+
+            # Checkear si going "up" es posible
+            if "up" in grid.actions(node.state):
+                # Obtener el succesor  y su costo
+                successor = grid.result(node.state, "up")
+                cost = node.cost + grid.individual_cost(node.state, "up")
+
+                # Checkear si el sucesor está en alcanzados o si el costo es menor
+                if successor not in reached or cost < reached[successor]:
+                    # Initialize the son node
+                    son = Node(
+                        "",
+                        successor,
+                        cost=cost,
+                        parent=node,
+                        action="up",
+                    )
+
+                    # Cargar el sucesor como alcanzado con su costo
+                    reached[successor] = cost
+                    #Encolar el nodo en la frontera con su costo
+                    frontier.add(son, cost)
+
+            # Checkear si going "left" es posible
+            if "left" in grid.actions(node.state):
+                # Obtener el succesor  y su costo
+                successor = grid.result(node.state, "left")
+                cost = node.cost + grid.individual_cost(node.state, "left")
+
+                # Checkear si el sucesor está en alcanzados o si el costo es menor
+                if successor not in reached or cost < reached[successor]:
+                    # Initialize the son node
+                    son = Node(
+                        "",
+                        successor,
+                        cost=cost,
+                        parent=node,
+                        action="left",
+                    )
+
+                    # Cargar el sucesor como alcanzado con su costo
+                    reached[successor] = cost
+                    #Encolar el nodo en la frontera con su costo
+                    frontier.add(son, cost)
+
+            # Checkear si going "down" es posible
+            if "down" in grid.actions(node.state):
+                # Obtener el succesor  y su costo
+                successor = grid.result(node.state, "down")
+                cost = node.cost + grid.individual_cost(node.state, "down")
+
+                # Checkear si el sucesor está en alcanzados o si el costo es menor
+                if successor not in reached or cost < reached[successor]:
+                    # Initialize the son node
+                    son = Node(
+                        "",
+                        successor,
+                        cost=cost,
+                        parent=node,
+                        action="down",
+                    )
+
+                    # Cargar el sucesor como alcanzado con su costo
+                    reached[successor] = cost
+                    #Encolar el nodo en la frontera con su costo
+                    frontier.add(son, cost)
+
